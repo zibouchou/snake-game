5d42a30d2c085fc3ae0f467e030df41f119805d6
KONE <aziz.k@monvanityideal.com>
2021-04-06 16:51:35 +0200

    create snake + change background color and size

--- snake.py
+++ snake.py
@@ -11,9 +11,14 @@ import sys
 
 
 pygame.init()
-showScreen = pygame.display.set_mode((500,250))
-pygame.display.update()
+showScreen = pygame.display.set_mode((800,600))
+
 pygame.display.set_caption('Aziz Snake game')
+
+backColor = (255,255,255)
+snakeColor = (0,0,255)
+foodColor = (255,0,0)
+
 gamePlay = False
 
 while not gamePlay:
@@ -21,6 +26,9 @@ while not gamePlay:
         print(e) #print actions
         if e.type == pygame.QUIT:
             gamePlay = True
+    showScreen.fill(backColor)
+    pygame.draw.rect(showScreen, snakeColor, [200,150,10,10])
+    pygame.display.update()
         
 
 pygame.quit()
