403ef022f4490aee05e5bb0a4795da7566e73487
KONE <aziz.k@monvanityideal.com>
2021-04-06 16:23:54 +0200

    add loop to keep screen active

--- snake.py
+++ snake.py
@@ -13,5 +13,13 @@ import sys
 pygame.init()
 showScreen = pygame.display.set_mode((500,250))
 pygame.display.update()
+pygame.display.set_caption('Aziz Snake game')
+gamePlay = True
+
+while gamePlay:
+    for e in pygame.event.get():
+        print(e) #print actions 
+        
+
 pygame.quit()
 sys.exit()
