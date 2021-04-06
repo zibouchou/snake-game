0b43bdc81f7fb4811c6d71b33a39b16ba5e34fe9
KONE <aziz.k@monvanityideal.com>
2021-04-06 17:09:09 +0200

    make the snake move

--- snake.py
+++ snake.py
@@ -21,14 +21,43 @@ foodColor = (255,0,0)
 
 gamePlay = False
 
+#coordinates
+
+x = 300
+y = 300
+
+#when coordinates will change
+newX = 0       
+newY = 0
+
+clock = pygame.time.Clock()
+
 while not gamePlay:
     for e in pygame.event.get():
         print(e) #print actions
         if e.type == pygame.QUIT:
             gamePlay = True
+        if e.type == pygame.KEYDOWN:
+            if e.key == pygame.K_LEFT:
+                newX = -10
+                newY = 0
+            elif e.key == pygame.K_RIGHT:
+                newX = 10
+                newY = 0
+            elif e.key == pygame.K_UP:
+                newX = 0
+                newY = -10
+            elif e.key == pygame.K_DOWN:
+                newX = 0
+                newY = 10
+    
+    x += newX
+    y += newY
     showScreen.fill(backColor)
-    pygame.draw.rect(showScreen, snakeColor, [200,150,10,10])
+    pygame.draw.rect(showScreen, snakeColor, [x,y,10,10])
     pygame.display.update()
+    
+    clock.tick(30)
         
 
 pygame.quit()
