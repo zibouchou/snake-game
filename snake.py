4ae391fdd822852364d6f708be9d3b3f0ace6989
KONE <aziz.k@monvanityideal.com>
2021-04-06 16:06:59 +0200

    init snake game

---
+++ snake.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  6 15:53:26 2021
+
+@author: Aziz
+"""
+
+
+import pygame
+import sys
+
+
+pygame.init()
+showScreen = pygame.display.set_mode((500,250))
+pygame.display.update()
+pygame.quit()
+sys.exit()
